1f7037d23ca6392bc25f4ef45f4d57d71b01b7e1
KTHIRTY <kthirty@163.com>
2024-08-08 23:28:50 +0800

    fix package

--- packages/Toolbar/Previews.tsx
+++ packages/Toolbar/Previews.tsx
@@ -16,7 +16,7 @@ const Previews = defineComponent({
       const modeler = getModeler()
       if (!modeler) return message('warning', '模型加载失败，请刷新重试')
       const { xml } = await modeler.saveXML({ format: true, preamble: true })
-
+      previewText.value = xml || ''
       Modal.info({
         title: '预览文件',
         icon: false,
@@ -34,7 +34,7 @@ const Previews = defineComponent({
         },
         content: () => (
           <div class="preview-model" style={{ height: '100%' }}>
-            <Textarea bordered={false} readonly autoSize v-model:value={xml} />
+            <Textarea bordered={false} readonly autoSize v-model:value={previewText} />
           </div>
         )
       })
