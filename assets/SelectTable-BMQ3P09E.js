965e3e5d96032b040812080b47ec4660caaf7572
KTHIRTY <kthirty@163.com>
2024-11-25 22:23:16 +0800

    deploy

---
+++ assets/SelectTable-BMQ3P09E.js
@@ -0,0 +1,1 @@
+import{d as t,r as a,c as u,e as n,t as o,o as r}from"./index-EkXNvB0f.js";const d=t({name:"SelectTable",__name:"SelectTable",setup(c){const l=a("5"),s=a([]);for(let e=0;e<10;e++)s.value.push({value:e+"",label:"label"+e});return(e,v)=>(r(),u("div",null,[n("div",null,"当前值为："+o(l.value),1)]))}});export{d as default};
